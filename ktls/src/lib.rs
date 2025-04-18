4d9f1efdbc8bbd67be0f8b33f86c63e02ec7d3ee
Hantong Chen <cxwdyx620@gmail.com>
2025-04-18 22:45:14 +0800

    fix: expose setup::TlsStream

--- ktls/src/lib.rs
+++ ktls/src/lib.rs
@@ -12,7 +12,7 @@ pub use cipher::{KtlsCipherSuite, KtlsCipherType};
 pub use error::{CipherSuiteError, Error};
 pub use ffi::CryptoInfo;
 #[allow(deprecated)]
-pub use setup::{config_ktls_client, config_ktls_server, Setup};
+pub use setup::{config_ktls_client, config_ktls_server, Setup, TlsStream};
 pub use stream::{cork::CorkStream, KtlsStream};
 pub use utils::{
     async_read_ready::AsyncReadReady,
